01ed4c3ca4991202363e8cb32400d276edbfbffc
bobbritel12 <britelyassine47@gmail.com>
2024-02-22 11:06:31 +0100

    Angular

--- chachat/src/app/app-routing.module.ts
+++ chachat/src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// Compoennts
+import { TasksComponent } from './component/tasks/tasks.component';
+import { SigninComponent } from './component/signin/signin.component';
+import { SignupComponent } from './component/signup/signup.component';
+import { PrivateTasksComponent } from './component/private-tasks/private-tasks.component';
+import { AuthGuard} from './auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+    path: '',
+    redirectTo: '/tasks',
+    pathMatch: 'full'
+  },
+  {
+    path: 'tasks',
+    component: TasksComponent
+  },
+  {
+    path: 'private',
+    component: PrivateTasksComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'signin',
+    component: SigninComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
