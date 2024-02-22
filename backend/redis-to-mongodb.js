7b0de49ca407e4c50d463afd5bddc5d2a252d955
bobbritel12 <britelyassine47@gmail.com>
2024-02-22 08:55:46 +0100

    Palomo

--- backend/redis-to-mongodb.js
+++ backend/redis-to-mongodb.js
@@ -1,13 +1,15 @@
-const redis = require('redis');
 const mongoose = require('mongoose');
+const redis = require('redis'); 
 
 // Connect to Redis
 const redisClient = redis.createClient();
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:37017', {
+mongoose.connect('mongodb://127.0.0.1:27018', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
    .then(db => console.log('Database is connected'))
     .catch(err => console.log(err));
+
+
