044d69cbab55a16c27a6d212e9607b4a939a00a2
nouhailabk <95505274+nouhailabk@users.noreply.github.com>
2024-03-04 16:58:40 +0100

    cc

--- chachat/src/app/app.module.ts
+++ chachat/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AuthGuard } from './auth.guard';
 import { TokenInterceptorService } from './services/token-interceptor.service';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 
 
@@ -23,7 +24,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     SignupComponent,
     TasksComponent,
     PrivateTasksComponent,
-    SigninComponent
+    SigninComponent,
+    UserProfileComponent
   ],
   imports: [
     FormsModule,
