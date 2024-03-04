8d801961e6361c70273cab042cdd91278ceae632
bobbritel12 <britelyassine47@gmail.com>
2024-03-04 14:38:43 +0100

    Yasser Modifications

--- chachat11/src/app/app.module.ts
+++ chachat11/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { ChatComponent } from './chat/chat.component';
-import { HistoryComponent } from './history/history.component';
+import { LoginComponent } from './component/login/login.component';
+import { UserListComponent } from './component/user-list/user-list.component';
+import { ChatComponent } from './component/chat/chat.component';
+import { HistoryComponent } from './component/history/history.component';
 
 @NgModule({
   declarations: [
